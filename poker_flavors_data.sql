07e7dfdcbd230400cc3c783fafffd3df82f176d8
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-05-23 20:02:53 -0400

    add poker_flavors_table.sql and poker_flavors_data.sql

---
+++ poker_flavors_data.sql
@@ -0,0 +1,59 @@
+use poker
+
+delete from poker_flavors;
+
+insert into poker_flavors (
+  poker_flavor,
+  poker_flavor_descr
+)
+values (
+  0,
+  'Pot Limit Hold''em'
+);
+
+insert into poker_flavors (
+  poker_flavor,
+  poker_flavor_descr
+)
+values (
+  1,
+  'Pot Limit Omaha'
+);
+
+insert into poker_flavors (
+  poker_flavor,
+  poker_flavor_descr
+)
+values (
+  2,
+  'Limit 7 Card Stud'
+);
+
+insert into poker_flavors (
+  poker_flavor,
+  poker_flavor_descr
+)
+values (
+  3,
+  'No Limit Hold''em'
+);
+
+insert into poker_flavors (
+  poker_flavor,
+  poker_flavor_descr
+)
+values (
+  4,
+  'Limit 5 Card Draw'
+);
+
+insert into poker_flavors (
+  poker_flavor,
+  poker_flavor_descr
+)
+values (
+  5,
+  'No Limit Triple Draw 2-7 Lowball'
+);
+
+quit
