0edc8ff2c9dfc2ca90ea5e13cd305a1791e15da3
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-08-24 09:10:05 -0400

    changes for sit and go tournaments

--- avg_delta_by_flavor2.sql
+++ avg_delta_by_flavor2.sql
@@ -1,10 +1,10 @@
 use poker
 
 select f.poker_flavor_descr,
-s.initial_stake_max,sum(s.delta),count(*),sum(s.delta) / count(*)
+s.initial_stake,sum(s.delta),count(*),sum(s.delta) / count(*)
 from poker_sessions s,poker_flavors f
 where s.poker_flavor = f.poker_flavor
-group by s.poker_flavor,s.initial_stake_max
-order by sum(s.delta) / count(*) desc,s.poker_flavor,s.initial_stake_max desc;
+group by s.poker_flavor,s.initial_stake
+order by sum(s.delta) / count(*) desc,s.poker_flavor,s.initial_stake desc;
 
 quit
