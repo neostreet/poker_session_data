6d156f9801d60576031c41260dc261bc70256804
Aidan Lloyd <lloyd.aidan@gmail.com>
2012-08-24 15:20:52 -0400

    add counts_tournament.sql

---
+++ counts_tournament.sql
@@ -0,0 +1,12 @@
+use poker
+
+select count(*) from poker_sessions
+where sit_and_go = 1 and delta < 0;
+
+select count(*) from poker_sessions
+where sit_and_go = 1 and delta > 0;
+
+select count(*) from poker_sessions
+where sit_and_go = 1;
+
+quit
