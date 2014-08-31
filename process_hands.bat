a589a78e52598349d6895c076a8cd40ddb0d2ef9
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-08-31 10:08:16 -0400

    catch up with local changes

--- process_hands.bat
+++ process_hands.bat
@@ -16,4 +16,5 @@ call runtot_int_ib_sae_it 50000 fdeltt.out
 call runtot_int_ib_sae_it 0 fdeltt.out
 call runtot_int_ib_sb_it 0 fdeltt.out
 call runtot_int_it fdeltt.out
+call underwater_count_it fdeltt.out.runtot_int
 call avgds0
