f0eca28d291d59873bb97a76216ab00283ea558d
Aidan Lloyd <lloyd.aidan@gmail.com>
2014-12-30 15:27:05 -0500

    catch up with local changes

--- mtt_8game_detail.sql
+++ mtt_8game_detail.sql
@@ -1,6 +1,6 @@
 use poker
 
-select delta,buy_in,entry_fee,place,winnings,poker_session_date
+select delta,buy_in,entry_fee,num_hands,place,winnings,poker_session_date
 from poker_sessions
 where poker_style = 2 and poker_flavor = 9
 order by id;
