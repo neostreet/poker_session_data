f0dc27fcb16f2486dec3ad63b35a255ab544fdd0
Aidan Lloyd <lloyd.aidan@gmail.com>
2015-01-11 11:43:37 -0500

    catch up with local changes

--- style_flavor.sql
+++ style_flavor.sql
@@ -1,10 +1,10 @@
 use poker
 
-select sum(s.delta),count(*),st.poker_style_descr,f.poker_flavor_descr
+select sum(s.delta),count(*),sum(s.delta) / count(*),st.poker_style_descr,f.poker_flavor_descr
 from poker_sessions s,poker_styles st,poker_flavors f
 where s.poker_style = st.poker_style and
 s.poker_flavor = f.poker_flavor
 group by s.poker_style,s.poker_flavor
-order by 1 desc,2 desc;
+order by 1 desc,s.poker_style,s.poker_flavor;
 
 quit
