c8445919ab339b9c2975198c3ce551d9af924beb
Aidan Lloyd <lloyd.aidan@gmail.com>
2017-07-01 10:04:57 -0400

    catch up with local changes

--- mtt_nlhe_paid_percentile0.sql
+++ mtt_nlhe_paid_percentile0.sql
@@ -3,7 +3,8 @@ use poker
 select (num_entries - places_paid) / num_entries,places_paid,num_entries,place,winnings,
 concat(poker_session_date,tournament_letter)
 from poker_sessions
-where poker_style = 2 and poker_flavor = 3 and places_paid is not null and num_entries is not null
+where poker_style = 2 and poker_flavor = 3 and
+num_entries is not null and places_paid is not null
 order by id;
 
 quit
